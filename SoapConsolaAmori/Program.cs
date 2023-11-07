a6fab0d8e4325013ef90dab057db02aa9034a8da
Vimakro <131078738+Vimakro@users.noreply.github.com>
2023-11-07 11:40:20 -0300

    Add files via upload

---
+++ SoapConsolaAmori/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoapConsolaAmori.ServicioAmori;
+
+namespace SoapConsolaAmori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioAmori.TipoCambio servicio = new ServicioAmori.TipoCambio();
+
+
+            var respuesta = servicio.TipoCambioDia();
+
+            if (respuesta != null )
+            {
+                Console.WriteLine(respuesta.CambioDolar.First().fecha);
+                Console.WriteLine(respuesta.CambioDolar.First().referencia);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("No hay respuesta");
+            }
+
+
+        }
+    }
+}
